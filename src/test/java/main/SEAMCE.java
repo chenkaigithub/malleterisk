06a30118f1c58b9ff92a0e570412ea8dc9370175
TT* <ttasterisco@gmail.com>
2011-02-18 20:35:09 +0000

    draft cross validation added

--- src/test/java/main/SEAMCE.java
+++ src/test/java/main/SEAMCE.java
@@ -2,14 +2,16 @@ package main;
 
 import java.io.File;
 import java.sql.SQLException;
+import java.util.Collection;
 import java.util.LinkedList;
-import java.util.Random;
 
 import cc.mallet.classify.Classifier;
 import cc.mallet.classify.ClassifierTrainer;
 import cc.mallet.classify.NaiveBayesTrainer;
+import cc.mallet.classify.Trial;
+import cc.mallet.types.Alphabet;
 import cc.mallet.types.InstanceList;
-import cc.mallet.types.MatrixOps;
+import cc.mallet.types.InstanceList.CrossValidationIterator;
 import fs.FeatureTransformationPipeline;
 import fs.IFeatureTransformer;
 import fs.methods.PruneByDF;
@@ -45,60 +47,71 @@ public class SEAMCE {
 //		IDataSet ds = new EnronDbDataSet(dal, 1, 6);
 //		InstanceList ilBody = new BodyPreProcessor().getInstanceList();
 //		//ilBody.save(new File("ilBody.txt"));
+//		//InstanceList shuffled = ilBody.shallowClone();
+//		//shuffled.shuffle (new Random()); // this is where the randomness happens
+//		//shuffled.save(new File("ilBodyShuffled.txt"));
 //
 //		System.out.println("-preprocess-");
 //		for (Instance msgInstance : ds) ilBody.addThruPipe(msgInstance);
 		
+		
+		
 		System.out.println("-loading documents-");
-		InstanceList ilBody = InstanceList.load(new File("ilBody.txt"));
+		InstanceList ilBody = InstanceList.load(new File("ilBodyShuffled.txt"));
+		
+		
+		// TODO:
+		Alphabet alphabet = ilBody.getAlphabet();
 		
 		System.out.println("-feature selection-");
-		System.out.println(ilBody.getAlphabet().size());
-		LinkedList<IFeatureTransformer> featureSelectors = new LinkedList<IFeatureTransformer>();
-		featureSelectors.add(new TFIDF());
-		featureSelectors.add(new PruneByTF(5, 100));
-		featureSelectors.add(new PruneByDF(5000));
-		featureSelectors.add(new RankByIG(2500));
-		featureSelectors.add(new PruneByL0Norm(1000));
-		featureSelectors.add(new RankByL0Norm(2500));
-		ilBody = new FeatureTransformationPipeline(featureSelectors).runThruPipeline(ilBody);
-		System.out.println(ilBody.getAlphabet().size());
-		
-		System.out.println("-classification-");
-		InstanceList[] bodySplit = ilBody.split(new double[] { 0.8, 0.2 });
-		
-		ClassifierTrainer<?> bodyTrainer = new NaiveBayesTrainer();
-		Classifier nbBodyClassifier = bodyTrainer.train(bodySplit[0]);
-		System.out.println("body training accuracy is " + nbBodyClassifier.getAccuracy(bodySplit[0]));
-		System.out.println("body testing accuracy is " + nbBodyClassifier.getAccuracy(bodySplit[1]));
+		// normalize the feature iteration (0% ... 100%)
+		final double step = alphabet.size() / 100.0;
+		for(int i = 0; i <= 100; i += 5) {
+			System.out.println(Math.ceil(i*step));
+			
+//			System.out.println(ilBody.getAlphabet().size());
+//			LinkedList<IFeatureTransformer> featureSelectors = new LinkedList<IFeatureTransformer>();
+////			featureSelectors.add(new TFIDF());
+////			featureSelectors.add(new PruneByTF(5, 100));
+////			featureSelectors.add(new PruneByDF(2500));
+//			featureSelectors.add(new RankByIG(i));
+////			featureSelectors.add(new PruneByL0Norm(2500));
+////			featureSelectors.add(new RankByL0Norm(2500));
+//			ilBody = new FeatureTransformationPipeline(featureSelectors).runThruPipeline(ilBody);
+//			System.out.println(ilBody.getAlphabet().size());
+//			
+//			System.out.println("-classification-");
+//			int numFolds = 10;
+//			double v = 0;
+//			Collection<Trial> trials = crossValidate(ilBody, numFolds, new NaiveBayesTrainer());
+//			for (Trial trial : trials) v += trial.getAccuracy();
+//			System.out.println("average accuracy: " + (v/numFolds));
+////			System.out.println(new ConfusionMatrix(trial));
+//			break;
+		}
 	}
 	
-	// TODO: code from InstanceList; modify it for a deterministic instance list splitting
-	public static void splitInstances(InstanceList instances, double[] proportions) {
-		// InstanceList.split
-		InstanceList shuffled = instances.shallowClone();
-		shuffled.shuffle (new Random()); // this is where the randomness happens
-
-		// InstanceList.splitInOrder
-		InstanceList[] ret = new InstanceList[proportions.length];
-		double maxind[] = proportions.clone();
-		MatrixOps.normalize(maxind);
-		for (int i = 0; i < maxind.length; i++) {
-			ret[i] = instances.cloneEmpty();  // Note that we are passing on featureSelection here.
-			if (i > 0) 
-				maxind[i] += maxind[i-1];
-		}
-		for (int i = 0; i < maxind.length; i++) { 
-			// Fill maxind[] with the highest instance index to go in each corresponding returned InstanceList
-			maxind[i] = Math.rint (maxind[i] * instances.size());
-		}
-		for (int i = 0, j = 0; i < instances.size(); i++) {
-			// This gives a slight bias toward putting an extra instance in the last InstanceList.
-			while (i >= maxind[j] && j < ret.length) 
-				j++;
-			ret[j].add(instances.get(i));
+	/**
+	 * Performs cross validation to an instance list.
+	 * Returns the results of all the runs in the form of trial objects.
+	 * 
+	 * @param instances
+	 * @param numFolds
+	 * @param trainer
+	 * @return
+	 */
+	public static final Collection<Trial> crossValidate(InstanceList instances, int numFolds, ClassifierTrainer<?> trainer) {
+		LinkedList<Trial> trials = new LinkedList<Trial>();
+		
+		CrossValidationIterator cvi = instances.crossValidationIterator(numFolds);
+		InstanceList[] folds = null;
+		Classifier classifier = null;
+		while(cvi.hasNext()) {
+			folds = cvi.next();
+			classifier = trainer.train(folds[0]);
+			trials.add(new Trial(classifier, folds[1]));
 		}
 		
-		// return ret;
+		return trials;
 	}
 }
