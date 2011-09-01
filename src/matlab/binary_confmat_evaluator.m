0003dcb11e914619d8901e9a836cfe257a955f4c
TT* <ttasterisco@gmail.com>
2011-09-01 18:25:26 +0100

    one versus all is reborn

--- src/matlab/binary_confmat_evaluator.m
+++ src/matlab/binary_confmat_evaluator.m
@@ -1,10 +1,11 @@
 clear;
 files = uipickfiles('FilterSpec', '/Work/msc/code/malleterisk-test/results');
-num_files = size(files, 1);
+num_files = size(files, 2);
 
 res_cm = zeros(2, 2);
 for i = 1:num_files
     cm = confusion_matrix(files{i});
+    cm(isnan(cm)) = 0; % replace NaNs with 0s
     res_cm = res_cm + cm;
 end
 
