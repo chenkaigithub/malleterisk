a9d0bebdb2707051d2153f19c00b423a94f296d3
TT* <ttasterisco@gmail.com>
2011-09-13 03:47:34 +0100

    classifier combination

--- src/test/java/experiments/CombinationExperiment.java
+++ src/test/java/experiments/CombinationExperiment.java
@@ -1,9 +1,30 @@
 package experiments;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import utils.classification.CombinationUtils;
+import cc.mallet.classify.ClassifierTrainer;
+import cc.mallet.classify.MaxEntTrainer;
+import cc.mallet.types.InstanceList;
+import classifiers.MajorityVotingClassifier;
+import execution.ExecutionResult;
+
 public class CombinationExperiment {
-	public static void main(String[] args) {
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) throws FileNotFoundException {
+		InstanceList[] instancelists = new InstanceList[2];
+		instancelists[0] = InstanceList.load(new File("instances+1+1+body"));
+		instancelists[1] = InstanceList.load(new File("instances+1+1+subject"));
 		
+		@SuppressWarnings("rawtypes")
+		ClassifierTrainer[] classifiers = new ClassifierTrainer[2];
+		classifiers[0] = new MaxEntTrainer();
+		classifiers[1] = new MaxEntTrainer();
 		
-		
+		ExecutionResult r = new ExecutionResult("instances+1+1", null, null, MajorityVotingClassifier.class.getSimpleName());
+		r.trials.put(0, CombinationUtils.xvClassify(instancelists, classifiers, 10));
+		r.outputTrials();
+		r.outputAccuracies();
 	}
 }
