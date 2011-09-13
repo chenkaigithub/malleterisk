14d504acdf4a956c561eca1fef6933d9c8445f0a
TT* <ttasterisco@gmail.com>
2011-09-13 12:47:34 +0100

    classifier combination

--- src/test/java/experiments/CombinationExperiment.java
+++ src/test/java/experiments/CombinationExperiment.java
@@ -8,19 +8,24 @@ import cc.mallet.classify.ClassifierTrainer;
 import cc.mallet.classify.MaxEntTrainer;
 import cc.mallet.types.InstanceList;
 import classifiers.MajorityVotingClassifier;
+import classifiers.PeoplefierTrainer;
 import execution.ExecutionResult;
 
 public class CombinationExperiment {
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws FileNotFoundException {
-		InstanceList[] instancelists = new InstanceList[2];
-		instancelists[0] = InstanceList.load(new File("instances+1+1+body"));
-		instancelists[1] = InstanceList.load(new File("instances+1+1+subject"));
+		InstanceList[] instancelists = new InstanceList[] {
+			InstanceList.load(new File("instances+1+1+body")),
+			InstanceList.load(new File("instances+1+1+subject")),
+			InstanceList.load(new File("instances+1+1+peoplefier"))
+		};
 		
 		@SuppressWarnings("rawtypes")
-		ClassifierTrainer[] classifiers = new ClassifierTrainer[2];
-		classifiers[0] = new MaxEntTrainer();
-		classifiers[1] = new MaxEntTrainer();
+		ClassifierTrainer[] classifiers = new ClassifierTrainer[] {
+			new MaxEntTrainer(),
+			new MaxEntTrainer(),
+			new PeoplefierTrainer()
+		};
 		
 		ExecutionResult r = new ExecutionResult("instances+1+1", null, null, MajorityVotingClassifier.class.getSimpleName());
 		r.trials.put(0, CombinationUtils.xvClassify(instancelists, classifiers, 10));
