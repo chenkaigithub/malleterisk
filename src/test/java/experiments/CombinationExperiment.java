3c02dd79ee67eec495a9695d8460244317cf26d2
TT* <ttasterisco@gmail.com>
2011-09-15 04:03:18 +0100

    updating classifier stuff

--- src/test/java/experiments/CombinationExperiment.java
+++ src/test/java/experiments/CombinationExperiment.java
@@ -9,27 +9,33 @@ import cc.mallet.classify.MaxEntTrainer;
 import cc.mallet.types.InstanceList;
 import classifiers.MajorityVotingClassifier;
 import classifiers.PeoplefierTrainer;
+import classifiers.TopicModelTrainer;
 import execution.ExecutionResult;
 
 public class CombinationExperiment {
 	public static void main(String[] args) throws FileNotFoundException {
-		
-		InstanceList[] instancelists = new InstanceList[] {
-			InstanceList.load(new File("instances+1+1+body")),
-			InstanceList.load(new File("instances+1+1+subject")),
-			InstanceList.load(new File("instances+1+1+topics-body")),
-			InstanceList.load(new File("instances+1+1+topics-subject")),
-			InstanceList.load(new File("instances+1+1+peoplefier"))
+		InstanceList[][] instancelists = new InstanceList[][] {
+			new InstanceList[] { InstanceList.load(new File("instances+1+1+body")),	InstanceList.load(new File("instances+1+1+subject")), InstanceList.load(new File("instances+1+1+topics-body")), InstanceList.load(new File("instances+1+1+topics-subject")), InstanceList.load(new File("instances+1+1+peoplefier")) },
+			new InstanceList[] { InstanceList.load(new File("instances+2+2+body")),	InstanceList.load(new File("instances+2+2+subject")), InstanceList.load(new File("instances+2+2+topics-body")), InstanceList.load(new File("instances+2+2+topics-subject")), InstanceList.load(new File("instances+2+2+peoplefier")) },
+			new InstanceList[] { InstanceList.load(new File("instances+2+3+body")),	InstanceList.load(new File("instances+2+3+subject")), InstanceList.load(new File("instances+2+3+topics-body")), InstanceList.load(new File("instances+2+3+topics-subject")), InstanceList.load(new File("instances+2+3+peoplefier")) },
+			new InstanceList[] { InstanceList.load(new File("instances+2+4+body")),	InstanceList.load(new File("instances+2+4+subject")), InstanceList.load(new File("instances+2+4+topics-body")), InstanceList.load(new File("instances+2+4+topics-subject")), InstanceList.load(new File("instances+2+4+peoplefier")) },
+			new InstanceList[] { InstanceList.load(new File("instances+2+5+body")),	InstanceList.load(new File("instances+2+5+subject")), InstanceList.load(new File("instances+2+5+topics-body")), InstanceList.load(new File("instances+2+5+topics-subject")), InstanceList.load(new File("instances+2+5+peoplefier")) },
+			new InstanceList[] { InstanceList.load(new File("instances+2+6+body")),	InstanceList.load(new File("instances+2+6+subject")), InstanceList.load(new File("instances+2+6+topics-body")), InstanceList.load(new File("instances+2+6+topics-subject")), InstanceList.load(new File("instances+2+6+peoplefier")) },
+			new InstanceList[] { InstanceList.load(new File("instances+2+7+body")),	InstanceList.load(new File("instances+2+7+subject")), InstanceList.load(new File("instances+2+7+topics-body")), InstanceList.load(new File("instances+2+7+topics-subject")), InstanceList.load(new File("instances+2+7+peoplefier")) },
+			new InstanceList[] { InstanceList.load(new File("instances+2+8+body")),	InstanceList.load(new File("instances+2+8+subject")), InstanceList.load(new File("instances+2+8+topics-body")), InstanceList.load(new File("instances+2+8+topics-subject")), InstanceList.load(new File("instances+2+8+peoplefier")) }
 		};
 		
 		@SuppressWarnings("rawtypes")
 		ClassifierTrainer[] classifiers = new ClassifierTrainer[] {
 			new MaxEntTrainer(),
 			new MaxEntTrainer(),
+			new TopicModelTrainer(),
+			new TopicModelTrainer(),
 			new PeoplefierTrainer()
 		};
 		
-		x(instancelists, classifiers);
+		for (int i = 0; i < instancelists.length; i++)
+			x(instancelists[i], classifiers);
 	}
 	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
